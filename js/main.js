0ab84b41f19cf4afb1225c21f72f92d88f172f4e
Andrea Torelli <gioran326@protonmail.com>
2023-03-01 06:49:10 +0100

    thumbnail con click

--- js/main.js
+++ js/main.js
@@ -72,87 +72,20 @@ prevDom.addEventListener("click",
    }
 )
 
-//DA RIVEDERE A MENTE FREDDA
+for (let i = 0; i < imageArray.length; i++) {
 
-// for (let i = 0; i < imageArray.length; i++) {
+   const pippo = document.getElementById(`thumbnail${i}`);
 
-//    const thumbnail1 = document.getElementById("thumbnail1");
+   let pluto = pippo;
 
-//    thumbnail1.addEventListener("click",
-//       function(){
+   pluto.addEventListener("click",
+      function(){
          
-//          addImageWrapperDom[currentImage].classList.remove("show");
-//          thumbnail[currentImage].classList.add("brightness");
-//          currentImage = 1;
-//          addImageWrapperDom[currentImage].classList.add("show");
-//          thumbnail[currentImage].classList.remove("brightness");
-//       }
-//    )
-// }
-
-// QUESTO È UN ACCROCCHIO, LO SO... DOMANI LO MIGLIORO!
-
-const thumbnail0 = document.getElementById("thumbnail0");
-
-thumbnail0.addEventListener("click",
-   function(){
-      
-      addImageWrapperDom[currentImage].classList.remove("show");
-      thumbnail[currentImage].classList.add("brightness");
-      currentImage = 0;
-      addImageWrapperDom[currentImage].classList.add("show");
-      thumbnail[currentImage].classList.remove("brightness");
-   }
-)
-
-const thumbnail1 = document.getElementById("thumbnail1");
-
-thumbnail1.addEventListener("click",
-   function(){
-      
-      addImageWrapperDom[currentImage].classList.remove("show");
-      thumbnail[currentImage].classList.add("brightness");
-      currentImage = 1;
-      addImageWrapperDom[currentImage].classList.add("show");
-      thumbnail[currentImage].classList.remove("brightness");
-   }
-)
-
-const thumbnail2 = document.getElementById("thumbnail2");
-
-thumbnail2.addEventListener("click",
-   function(){
-      
-      addImageWrapperDom[currentImage].classList.remove("show");
-      thumbnail[currentImage].classList.add("brightness");
-      currentImage = 2;
-      addImageWrapperDom[currentImage].classList.add("show");
-      thumbnail[currentImage].classList.remove("brightness");
-   }
-)
-
-const thumbnail3 = document.getElementById("thumbnail3");
-
-thumbnail3.addEventListener("click",
-   function(){
-      
-      addImageWrapperDom[currentImage].classList.remove("show");
-      thumbnail[currentImage].classList.add("brightness");
-      currentImage = 3;
-      addImageWrapperDom[currentImage].classList.add("show");
-      thumbnail[currentImage].classList.remove("brightness");
-   }
-)
-
-const thumbnail4 = document.getElementById("thumbnail4");
-
-thumbnail4.addEventListener("click",
-   function(){
-      
-      addImageWrapperDom[currentImage].classList.remove("show");
-      thumbnail[currentImage].classList.add("brightness");
-      currentImage = 4;
-      addImageWrapperDom[currentImage].classList.add("show");
-      thumbnail[currentImage].classList.remove("brightness");
-   }
-)
+         addImageWrapperDom[currentImage].classList.remove("show");
+         thumbnail[currentImage].classList.add("brightness");
+         currentImage = i;
+         addImageWrapperDom[currentImage].classList.add("show");
+         thumbnail[currentImage].classList.remove("brightness");
+      }
+   )
+}
